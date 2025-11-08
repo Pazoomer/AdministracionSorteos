30f187530ade91e7ad5e6041c2fa4769907a2929
Jorge Zamora <jorge.zamora245103@potros.itson.edu.mx>
2025-11-08 12:42:30 -0700

    Pruebas de entorno Z

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AdministracionSorteos/', 
+  base: 'https://pazoomer.github.io/AdministracionSorteos/',
 })
