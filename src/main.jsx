a1a3d7407e73815d42d3f45e400d9c78f6e20e1a
Jorge Zamora <jorge.zamora245103@potros.itson.edu.mx>
2025-11-08 12:25:26 -0700

    Prueba de entorno X

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/AdministracionSorteos">
     <App />
   </BrowserRouter>
 );
